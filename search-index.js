25040dbbd826af1b69ac4aaf7810fe7e7757ddcd
KGrewal1 <45569241+KGrewal1@users.noreply.github.com>
2023-10-29 17:12:17 +0000

    Deploying to gh-pages from @ KGrewal1/deploy-test@f14e6eb767e6b1ade4ed565219b9ad60a50bd518 🚀

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"deploy_test":{"doc":"Test gh-pagesdeployment","t":"F","n":["add"],"q":[[0,"deploy_test"]],"d":["my function is"],"i":[0],"f":[[[1,1],1]],"c":[],"p":[[15,"usize"]],"b":[]}\
+"deploy_test":{"doc":"Test gh-pagesdeployment","t":"F","n":["add"],"q":[[0,"deploy_test"]],"d":["my function is"],"i":[0],"f":[[[1,1],1]],"c":[],"p":[[15,"usize"]]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
